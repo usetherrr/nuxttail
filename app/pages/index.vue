6bb1cef14c0f7fdffefdd06e73c4ce7f8bb1b10d
usetherrr <141813344+usetherrr@users.noreply.github.com>
2024-09-29 04:01:49 +0800

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+  import { heroImageUrl } from '@/utils/hero'
+
+  definePageMeta({
+    // layout: 'default',
+    // name: 'index',
+    // alias: 'index',
+    title: 'Home',
+    description: 'Sip, Savor, and Spark Ideas at Cafe Bella Vista!',
+    navOrder: 1,
+    type: 'primary',
+    icon: 'i-mdi-home',
+    // ogImage: 'images/ogImage.png', // url or local images inside public folder, for eg, ~/public/images/ogImage.png
+  })
+
+  const { optimizeImage } = useOptimizeImage()
+  const heroImageOptimized = {
+    alt: `hero`,
+    cover: true,
+    ...optimizeImage(
+      heroImageUrl,
+      /* options */
+      {
+        // placeholder: false, // placeholder image before the actual image is fully loaded.
+      },
+      true /* return bgStyles */,
+    ),
+  }
+
+  const heroImage = heroImageOptimized.src
+  const bgStyles = heroImageOptimized.bgStyles
+</script>
+<template>
+  <div>
+    <HeroSection />
+    <AboutTemplate />
+    <WalkThrough />
+  </div>
+</template>
+<style scoped></style>
